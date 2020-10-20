eefae9293ab6e25914b86e867b33eabf56611b26
Korawit <korawit.sit@mail.kmutt.ac.th>
2020-10-20 16:52:02 +0700

    update to testbranch

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -7,10 +7,16 @@ public class Gittest {
    
     public static void main(String[] args) {
         sayHello();
-    
+        goodBye();
 
     }
         public static void sayHello(){
             System.out.println("Hello");   
+        }
+        public static void goodBye(){
+            System.out.println("Goodbye");
+        }
+        public static void sayHi(){
+            System.out.println("Hi");
         }
 }
